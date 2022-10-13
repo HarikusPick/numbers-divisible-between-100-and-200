47c24b68f51792f34f8a0c1aafaa05a2447cfa08
Yunus Furkan Özer <bardakta.kahve@gmail.com>
2022-10-13 22:30:11 +0300

    first commit

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int adet =0,sayi;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Değeri Giriniz.");
+                sayi = Convert.ToInt32(Console.ReadLine());
+                if ((sayi > 100) && (sayi < 200))
+                    adet++;
+            } 
+            Console.WriteLine(adet);
+            Console.ReadKey();
+        }
+    }
+}
+ 
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
